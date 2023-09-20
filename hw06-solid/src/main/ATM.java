068e18ee4309bf184508bfd52f35677b9cb0f4e2
luminorena <94684347+luminorena@users.noreply.github.com>
2023-09-20 15:48:34 +0300

    #3 fix hw06-solid

--- hw06-solid/src/main/ATM.java
+++ hw06-solid/src/main/ATM.java
@@ -1,9 +1,13 @@
 package main;
 
 
+import java.util.HashMap;
+
 public interface ATM {
 
     void withdrawMoney(int bankNotes);
+
+    void depositMoney(HashMap<Integer, Integer> map);
 
     void calculateRest();
 
--- hw06-solid/src/main/ATMRunner.java
+++ hw06-solid/src/main/ATMRunner.java
@@ -1,47 +1,40 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Supplier;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class ATMRunner implements ATM {
-    private final int initialAtmSum;
+
     private int bankNotesR;
 
-    public ATMRunner(int initialAtmSum) {
-        this.initialAtmSum = initialAtmSum;
+    HashMap<Integer, Integer> map;
 
+    public ATMRunner(HashMap<Integer, Integer> map) {
+        this.map = map;
     }
 
     @Override
-    public void withdrawMoney(int bankNotes) {
+    public void withdrawMoney(int sumToWithdraw) {
         try {
 
-            bankNotesR = bankNotes;
-            Integer result1 = 0;
+            bankNotesR = sumToWithdraw;
 
-            Optional<Integer> minValue = Arrays.stream(BankNotes.values())
-                    .map(BankNotes::getValue).min(Integer::compareTo);
+            Optional<Integer> minValue = map.entrySet()
+                    .stream()
+                    .min(Comparator.comparingInt(Map.Entry::getKey))
+                    .map(Map.Entry::getKey);
 
-            long countValues = Arrays.stream(BankNotes.values())
-                    .map(BankNotes::genCount).count();
-
-            Supplier<Stream<Integer>> streamSupplier
-                    = () -> Stream.of(BankNotes.values())
-                    .map(BankNotes::getValue);
             List<Integer> withdrawList = new ArrayList<>();
-            while (bankNotes >= minValue.get()) {
-                int finalInitialValue = bankNotes;
-                long finalCountValues = countValues;
-                result1 = streamSupplier.get()
-                        .filter(x -> x <= finalInitialValue && finalCountValues > 0).max(Integer::compareTo)
-                        .stream().findFirst().orElse(null);
-                bankNotes -= result1;
-                countValues--;
-                withdrawList.add(result1);
+            while (sumToWithdraw >= minValue.get()) {
+                int finalInitialValue = sumToWithdraw;
+                if (getStream()
+                        .filter(x -> x <= finalInitialValue).count() == 0)
+                    break;
+                int bankNote = getStream().filter(x -> x <= finalInitialValue)
+                        .max(Integer::compareTo).orElse(0);
+                sumToWithdraw -= bankNote;
+                map.put(bankNote, map.get(bankNote) - 1);
+                withdrawList.add(bankNote);
             }
             System.out.println("Сумма выдана минимальным количеством банкнот: " + withdrawList);
         } catch (NullPointerException e) {
@@ -50,9 +43,30 @@ public class ATMRunner implements ATM {
 
     }
 
+    private Stream<Integer> getStream() {
+        return map.
+                entrySet().stream().filter(x -> x.getValue() > 0).map(Map.Entry::getKey);
+    }
+
+    @Override
+    public void depositMoney(HashMap<Integer, Integer> input) {
+        for (Map.Entry<Integer, Integer> entry : input.entrySet()) {
+            if (entry.getValue() < 0)
+                throw new NoMoneyToWithdrawException("Как можно внести отрицательное количество?");
+            if (map.containsKey(entry.getKey()))
+                map.put(entry.getKey(), map.get(entry.getKey()) + entry.getValue());
+            else
+                throw new NoMoneyToWithdrawException("Нет такого номинала " + entry.getKey() + "!");
+        }
+    }
+
+
     @Override
     public void calculateRest() {
-        int rest = initialAtmSum - bankNotesR;
+        int rest = 0;
+        for (Map.Entry<Integer, Integer> x : map.entrySet())
+            rest += x.getKey() * x.getValue();
+
         if (rest >= 0) {
             System.out.println("Остаток на счету: " + rest);
         } else throw new NoMoneyToWithdrawException("Запрошенная сумма больше первоначальной. Выдача невозможна.");
